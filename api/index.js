ec302530ad17bf555694802885efb121b4d913be
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-11-06 18:24:19 +0000

    connecting MongoDB

--- api/index.js
+++ api/index.js
@@ -12,7 +12,17 @@ app.use("/",(req,res)=>{
 
 */
 
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
 
 app.listen("3001", () => {
     console.log("BackEnd is running")
