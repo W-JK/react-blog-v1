444e84f191d2e6595ca6d9ec893e0ffccfd20694
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-11-09 19:54:38 +0000

    Login & credentials check

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -29,8 +29,24 @@ router.post("/register", async (req,res)=>{
 
 
 // -------------------- LOGIN    -------------------
+router.post("/login", async (req,res)=>{
+    try{
+       
+        const user = await User.findOne({
+        username: req.body.username
+        });
+        !user && res.status(400).json("Wrong credentioals")
+
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        !validated && res.status(400).json("Wrong credentioals") 
+        
+        res.status(200).json(user)
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
 
 
 
-
+// ------------------------------------------------------------
 module.exports = router
